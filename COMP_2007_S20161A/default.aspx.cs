ced26d45903a844ea10296c907405258109b8fe8
moetime <morrice_pfeiffer@hotmail.com>
2016-05-18 13:41:41 -0400

    Update nav bar. Added web form

---
+++ COMP_2007_S20161A/default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP_2007_S2016_Leeson1A
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            FullNameTextBox.Text = FirstNameTextBox.Text.ToString() + " " + LastNameTextBox.Text.ToString();
+        }
+    }
+}
